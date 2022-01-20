cfdc874e765ea3fa92e1f8884ffd2299417bd8cd
root <root@beta.jaxcorp.com>
2022-01-20 21:29:12 +0000

    style change

--- includes/sideBar_logIn.php
+++ includes/sideBar_logIn.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
   <div class="container-fluid"> <a class="navbar-brand" href="<?=$home_link?>"><img src="img/jax_corp.svg" width="150"></a>
     <div class="mobhide ml-auto" id="">
+    <a href="#" class="btn btn-info my-2 my-sm-0 mr-3 border-radius pt-2 pb-2"  data-toggle="modal" data-target=".bd-example-modal-lg"  >Donate</a>
+         <a href="<?=$ubi_status_link?>" class="btn my-2 my-sm-0 mr-3 border-radius pt-2 pb-2 btn-success" >Collect UBI</a>
       <form class="form-inline my-2 my-lg-0 homeselect mr-3">
         <div class="dropdown">
           <a id="btn_connect" href="#" class="btn btn-info my-2 my-sm-0 mr-3 border-radius pt-2 pb-2"    onclick="connect_wallet()">Connect a wallet</a>
