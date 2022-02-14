c6dd1452d731eda280c975c0dabe448208bba5ad
root <root@beta.jaxcorp.com>
2022-02-14 15:51:32 +0000

    fix: wnxj logo

--- includes/modal.php
+++ includes/modal.php
@@ -7,6 +7,14 @@
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
         </div>
         <div class="modal-body">
+          <div class="mobilemodalheight">
+              <div class="text-blue font-weight-bold text-center mb-4">
+              Please send WJAX to this donation address and we will add it to the UBI pool.
+              </div>
+              <button class="btn-block btn btn-lightgrey"> 0x4b0A58380f62cF451a4b098A4514531C102c1be2</button>
+          </div>
+      </div>
+        <!-- <div class="modal-body">
             <div class="mobilemodalheight modal_amoutn_form" >
               <form class="modal_amoutn_form_inner"> 
                   <label for="donate" class="text-blue font-weight-bold text-center mb-4">Amount</label>
@@ -14,7 +22,7 @@
                   <button class="btn btn-info btn-lg mx-w-300" id="btn_donate" onclick="donate();">Donate</button>
               </form>
             </div>
-        </div>
+        </div> -->
       </div>
     </div>
 </div>
