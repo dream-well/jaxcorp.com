b7af23b217f19e85c7dd1a8dde67091dcf5d7c38
Vinod M <vinod@jax.net>
2022-02-15 12:45:39 +0400

    video link fix

--- index.php
+++ index.php
@@ -154,7 +154,7 @@
             <div id="collapseOne" aria-labelledby="headingOne" data-parent="#accordionExample" class="collapse">
               <div class="card-body p-5">
                 <p class="font-weight-light m-0 blueclr">In simple words, universal basic income (UBI) is a minimum amount of money enough to cover all basic human needs.
-Learn more about UBI: [video link].
+Learn more about UBI: <a href = "https://youtu.be/5r3DS7oQoHY">Watch Video</a>.
  </p>
               </div>
             </div>
