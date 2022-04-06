55faf3d4ae18d72378433bc68f66f3f12a482999
Yarik <yaroslav@jax.net>
2022-04-06 18:00:40 +0400

    GG tag script

--- includes/head.php
+++ includes/head.php
@@ -1,10 +1,18 @@
 <!doctype html>
 <html lang="en">
 <head>
+
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="">
 <meta name="author" content="">
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NZC523H');</script>
+<!-- End Google Tag Manager -->
 <title>JaxCorp D.A.O</title>
 <link rel="icon" href="img/Group-752-150x150.png" sizes="32x32" />
 <link rel="icon" href="img/Group-752.png" sizes="192x192" />
@@ -29,4 +37,8 @@
 </head>
 
 <body class="bg-light">
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NZC523H"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div class="overlay"></div>
