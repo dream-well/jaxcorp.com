df6d4fb6d5d79420d3a871cee333bab8663c7a11
Yarik <yaroslav@jax.net>
2022-05-31 16:41:28 +0400

    mob

--- ubi_status.php
+++ ubi_status.php
@@ -142,25 +142,25 @@
           </div>
           <!--  -->
           <div class="bg-white box-shadow border-radius p-4 mb-4 voting">
-            <div class="flex-fill d-flex">
-              <div class="col-12 col-sm-8 d-flex justify-content-between">
+            <div class="flex-fill d-flex st_wrap">
+              <div class="col-12 col-md-8 d-flex justify-content-between">
                 <div class=" justify-content-start align-items-center">
                   <p class="text-info font80 mb-0 d-block">Total UBI Paid</p>
-                  <h3 class="d-block mb-0 pb-0 text-blue"><span class="total_ubi_paid">&nbsp;</span> <small>WJXN</small></h3>
+                  <h3 class="d-block mb-0 pb-0 text-blue"><span class="total_ubi_paid">&nbsp;</span> <small>WJAX</small></h3>
                 </div>
                 <div class=" justify-content-start align-items-center">
                   <p class="text-info font80 mb-0 d-block">Accumulated UBI</p>
                   <h3 class="d-block mb-0 pb-0 text-blue"><span class="pending_ubi">&nbsp;</span> <small>WJAX</small></h3>
                 </div>
               </div>
-              <div class="col-12 col-sm-4 d-flex justify-content-end align-items-center"><a href="#"
+              <div class="col-12 col-md-4 d-flex justify-content-end align-items-center"><a href="#"
                   class="btn btn-info btn-lg font80" onclick="collect_ubi()">Process UBI</a></div>
             </div>
           </div>
 
           <div class="bg-white box-shadow border-radius p-4 mb-4 voting">
-            <div class="flex-fill d-flex">
-              <div class="col-12 col-sm-8 d-flex justify-content-between">
+            <div class="flex-fill d-flex st_wrap">
+              <div class="col-12 col-md-8 d-flex justify-content-between">
               <div class=" justify-content-start align-items-center my-auto">
                 <p class="text-info font80 mb-0 d-block">UBI under-process</p>
                 <h3 class="d-block mb-0 pb-0 text-blue">
@@ -175,7 +175,7 @@
                 </p>
               </div>
               </div>
-              <div class="col-12 col-sm-4  d-flex justify-content-end align-items-center"><a href="https://t.me/jax_chat"
+              <div class="col-12 col-md-4  d-flex justify-content-end align-items-center"><a href="https://t.me/jax_chat"
                   class="btn btn-info btn-lg font80"><img src="img/telegram.svg" width="25px"
                     style="vertical-align: text-bottom" /> Open Telegram</a></div>
             </div>
